35ae87d75984e708c6f93ba8aeb7f335fc626f13
matlam <matz-josh@gmx.de>
2016-09-01 20:39:37 +0200

    add more fields to the item batch insert

--- admin/modules/bibliography/index.php
+++ admin/modules/bibliography/index.php
@@ -356,8 +356,8 @@ if (isset($_POST['saveData']) AND $can_read AND $can_write) {
         } else { $itemcode .= $b; }
         $itemcode .= $chars[1];
 
-        $item_insert_sql = sprintf("INSERT IGNORE INTO item (biblio_id, item_code, call_number, coll_type_id)
-        VALUES (%d, '%s', '%s', %d)", $updateRecordID?$updateRecordID:$last_biblio_id, $itemcode, $data['call_number'], $_POST['collTypeID']);
+        $item_insert_sql = sprintf("INSERT IGNORE INTO item (biblio_id, item_code, call_number, coll_type_id, item_status_id, input_date, last_update, uid)
+        VALUES (%d, '%s', '%s', %d, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, %d)", $updateRecordID?$updateRecordID:$last_biblio_id, $itemcode, $data['call_number'], $_POST['collTypeID'], $_SESSION['uid']);
         @$dbs->query($item_insert_sql);
       }
     }
