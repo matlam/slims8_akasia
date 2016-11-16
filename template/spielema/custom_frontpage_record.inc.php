e86ee888468b546ae5cc70ba647cc0c2c5c00944
matlam <matz-josh@gmx.de>
2016-11-16 01:07:36 +0100

    SpieleMA: show availability in opac search results
    custom for SpieleMA; don't merge

--- template/spielema/custom_frontpage_record.inc.php
+++ template/spielema/custom_frontpage_record.inc.php
@@ -8,5 +8,5 @@ $custom_fields['isbn_issn'] 	= array(0, __('ISBN/ISSN'));
 $custom_fields['collation'] 	= array(0, __('Collation'));
 $custom_fields['series_title'] 	= array(0, __('Series Title'));
 $custom_fields['call_number'] 	= array(0, __('Call Number'));
-$custom_fields['availability'] 	= array(0, __('Availability'));
+$custom_fields['availability'] 	= array(1, __('Availability'));
 ?>
